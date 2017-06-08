547ef44269def47f2cc0580a6c57c39eff53e532
pengking <351231093@qq.com>
2017-06-08 13:25:39 +0800

    add example
    add rtsp server example

--- example/main.cpp
+++ example/main.cpp
@@ -1,57 +1,101 @@
+/**********
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version. (See <http://www.gnu.org/copyleft/lesser.html>.)
+
+This library is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+**********/
+// Copyright (c) 1996-2017, Live Networks, Inc.  All rights reserved
+// LIVE555 Media Server
+// main program
+
+
 #include <BasicUsageEnvironment.hh>
-#include "liveMedia.hh"
-static ServerMediaSession* createNewSMS(UsageEnvironment& env,char const* fileName);
-int main()
-{
-	TaskScheduler* scheduler;
-	UsageEnvironment* env ;
+#include "simpleserver.h"
+
+int main(int argc, char** argv) {
+	// Begin by setting up our usage environment:
+	//i. ´´½¨Ò»¸ö»ù´¡µÄÊ¹ÓÃ»·¾³£¨BasicUsageEnvironment£©
+	//ii.´´½¨ÈÎÎñµ÷¶ÈÆ÷£¨TaskSchedulerÀà£©, ²¢°ó¶¨ÔÚÕâ¸öÊ¹ÓÃ»·¾³ÉÏ
+
+	TaskScheduler* scheduler = BasicTaskScheduler::createNew();
+	UsageEnvironment* env = BasicUsageEnvironment::createNew(*scheduler);
+	
+	//´æ´¢ÓÃ»§ÃûºÍÃÜÂëµÄÊı¾İ½á¹¹
+	UserAuthenticationDatabase* authDB = NULL;
+#ifdef ACCESS_CONTROL
+	// To implement client access control to the RTSP server, do the following:
+	authDB = new UserAuthenticationDatabase;
+	authDB->addUserRecord("username1", "password1"); // replace these with real strings
+	// Repeat the above with each <username>, <password> that you wish to allow
+	// access to the server.
+#endif
+
+  // Create the RTSP server.  Try first with the default port number (554),
+  // and then with the alternative port number (8554):
 	RTSPServer* rtspServer;
-	ServerMediaSession* sms;
-	//åˆ›å»ºä»»åŠ¡è°ƒç”¨å™¨
-	scheduler = BasicTaskScheduler::createNew();
-	//åˆ›å»ºäº¤äº’ç¯å¢ƒ
-	env = BasicUsageEnvironment::createNew(*scheduler);
-	//åˆ›å»ºRTSPæœåŠ¡å™¨
-	rtspServer = RTSPServer::createNew(*env,554); //ä½¿ç”¨554ç«¯å£
-	if(rtspServer == NULL)
-	{
-		rtspServer = RTSPServer::createNew(*env,8554); //554ç«¯å£è¢«å ç”¨ï¼Œå°±ä½¿ç”¨8554ç«¯å£
+	//serverµÄ¶Ë¿ÚºÅ
+	portNumBits rtspServerPortNum = 554;
+	//´´½¨RTSPµÄ·şÎñÆ÷£¨RTSPServer::createNew£©
+	rtspServer = simpleserver::createNew(*env, rtspServerPortNum, authDB);
+	if (rtspServer == NULL) {
+		//Èç¹ûÉÏÃæÃ»ÓĞ³É¹¦´´½¨server,ÄÇÃ´ºóÃæ¼ÌĞø»»Ò»¸ö¶Ë¿Ú£¬ÖØĞÂ´´½¨Ò»´Î
+		rtspServerPortNum = 8554;
+		rtspServer = simpleserver::createNew(*env, rtspServerPortNum, authDB);
 	}
-	//æ‰“å°æœåŠ¡å™¨åœ°å€
+	if (rtspServer == NULL) {
+		//server´´½¨Ê§°Ü£¬±¨´í£¬²¢ÍË³ö
+		*env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
+		exit(1);
+	}
+
+	*env << "LIVE555 Media Server\n";
+	//ÄÃµ½IPµØÖ·  Èç£ºrstp://192.168.2.183
+	//bind²Ù×÷Ò²ÔÚÕâ¸ö½×¶Î
+	char* urlPrefix = rtspServer->rtspURLPrefix(); 
 	*env << "Play streams from this server using the URL\n\t"
-	<< rtspServer->rtspURLPrefix() << "<filename>.\n";
-	//åˆ›å»ºsession
-	sms = createNewSMS(*env, "test_video.264");
-	rtspServer->addServerMediaSession(sms);
-	//æ·»åŠ å…¶å®ƒæ–‡ä»¶å¯¹åº”çš„session...
-	//è¿›è¡Œäº‹ä»¶å¾ªç¯
-	env->taskScheduler().doEventLoop(); // does not return
-	return 0;
+		<< urlPrefix << "<filename>\nwhere <filename> is a file present in the current directory.\n";
+	*env << "Each file's type is inferred from its name suffix:\n";
+	*env << "\t\".264\" => a H.264 Video Elementary Stream file\n";
+	*env << "\t\".265\" => a H.265 Video Elementary Stream file\n";
+	*env << "\t\".aac\" => an AAC Audio (ADTS format) file\n";
+	*env << "\t\".ac3\" => an AC-3 Audio file\n";
+	*env << "\t\".amr\" => an AMR Audio file\n";
+	*env << "\t\".dv\" => a DV Video file\n";
+	*env << "\t\".m4e\" => a MPEG-4 Video Elementary Stream file\n";
+	*env << "\t\".mkv\" => a Matroska audio+video+(optional)subtitles file\n";
+	*env << "\t\".mp3\" => a MPEG-1 or 2 Audio file\n";
+	*env << "\t\".mpg\" => a MPEG-1 or 2 Program Stream (audio+video) file\n";
+	*env << "\t\".ogg\" or \".ogv\" or \".opus\" => an Ogg audio and/or video file\n";
+	*env << "\t\".ts\" => a MPEG Transport Stream file\n";
+	*env << "\t\t(a \".tsx\" index file - if present - provides server 'trick play' support)\n";
+	*env << "\t\".vob\" => a VOB (MPEG-2 video with AC-3 audio) file\n";
+	*env << "\t\".wav\" => a WAV Audio file\n";
+	*env << "\t\".webm\" => a WebM audio(Vorbis)+video(VP8) file\n";
+	*env << "See http://www.live555.com/mediaServer/ for additional documentation.\n";
+
+	// Also, attempt to create a HTTP server for RTSP-over-HTTP tunneling.
+	// Try first with the default HTTP port (80), and then with the alternative HTTP
+	// port numbers (8000 and 8080).
+	//listenµÄ²Ù×÷Ò²ÔÚÕâ¸ö½×¶Î
+	
+	// if (rtspServer->setUpTunnelingOverHTTP(80) || rtspServer->setUpTunnelingOverHTTP(8000) || rtspServer->setUpTunnelingOverHTTP(8080)) {
+	// 	//´òÓ¡Õâ¸öserverµÄhttpµÄ¶Ë¿Ú
+	// 	*env << "(We use port " << rtspServer->httpServerPortNum() << " for optional RTSP-over-HTTP tunneling, or for HTTP live streaming (for indexed Transport Stream files only).)\n";
+	// }
+	// else {
+	// 	*env << "(RTSP-over-HTTP tunneling is not available.)\n";
+	// }
+	
+	//ËÀÑ­»·£¬´¦Àí¿É¶ÁµÄsocketºÍÊ±¼ä
+	env->taskScheduler().doEventLoop(); //does not return
+	return 0; // only to prevent compiler warning
 }
-static ServerMediaSession* createNewSMS(UsageEnvironment& env,char const* fileName)
-{
-	char const* extension = strrchr(fileName,'.');
-	ServerMediaSession* sms = NULL;
-	Boolean reuseSource = False; //ä¸é‡ç”¨source
-	sms = ServerMediaSession::createNew(env, fileName); //åˆ›å»ºä¸€ä¸ªsession
-	//
-	//åœ¨sessionä¸­æ·»åŠ subssesion
-	//
-	if(strcmp(extension, ".aac") == 0)
-	{
-		sms->addSubsession(ADTSAudioFileServerMediaSubsession::createNew(env, fileName, reuseSource));
-	}
-	else if(strcmp(extension, ".264") == 0)
-	{
-		sms->addSubsession(H264VideoFileServerMediaSubsession::createNew(env, fileName, reuseSource));
-	}
-	else if(strcmp(extension, ".mpg") == 0)
-	{
-		MPEG1or2FileServerDemux* demux
-		= MPEG1or2FileServerDemux::createNew(env, fileName, reuseSource);
-		sms->addSubsession(demux->newVideoServerMediaSubsession());
-		sms->addSubsession(demux->newAudioServerMediaSubsession());
-	}
-	//æ·»åŠ å…¶å®ƒåª’ä½“æ ¼å¼æ”¯æŒ...
-	return sms;
-}   
